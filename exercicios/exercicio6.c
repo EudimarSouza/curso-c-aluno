37bf5b0ad0a99f2576449d5ca152fe1f0b038106
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-15 20:14:55 -0300

    Update exercicio6.c
    Exercício calcular area do círculo

--- exercicios/exercicio6.c
+++ exercicios/exercicio6.c
@@ -1,1 +1,15 @@
 //Calcule a área de um círculo com base no raio informado. Como calcular: A = PI . R ao quadrado
+#include <stdio.h>
+
+int main(){
+    float a, r, real = 0;
+    const float pi = 3.141592654;
+
+    printf("Para saber a area do círculo, informe o raio: \n");
+    printf("Raio: ");
+    scanf("%f", &r);
+    a = pi * r * r;
+    printf("A area do círculo é %.2f\n", a);
+
+    return 0;
+}
