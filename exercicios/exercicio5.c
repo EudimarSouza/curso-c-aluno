e41e7ba680c83c9ec68288d12db438dbdc06873d
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-15 20:01:50 -0300

    Update exercicio5.c
    Exercício de calcula de área do quadrado

--- exercicios/exercicio5.c
+++ exercicios/exercicio5.c
@@ -1,1 +1,14 @@
 //Calcule a área de um quadrado. Como calcular: Area = lado * lado
+#include <stdio.h>
+
+int main(){
+    int lado, area = 0;
+
+    printf("Para saber a area do quadrado, informe o valor do Lado: \n");
+    printf("Lado: ");
+    scanf("%d", &lado);
+    area = lado * lado;
+    printf("O valor total da area é %d\n", area);
+
+    return 0;
+}
