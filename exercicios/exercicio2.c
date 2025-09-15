033f18db7aa08c86682033464954cce763e1ceac
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-14 22:55:11 -0300

    Update exercicio2.c
    Exercício 2 em C

--- exercicios/exercicio2.c
+++ exercicios/exercicio2.c
@@ -1,6 +1,17 @@
+//Perguntar o nome de quem está usando o programa e sua idade. Faça duas perguntas.
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    char nome[30]= "";
+    int idade = 0;
+
+    printf("Quem está usando o Programa? Informe o nome e idade \n");
+    
+    printf("Eu me chamo: ");
+    scanf("%s", nome);
+    printf("Minha idade é: ");
+    scanf("%d", &idade);
+    
+    printf("Usuário  %s.Idade %d. \n", nome, idade);
     return 0;
 }
