6fb405abf288164f8e1aefde9a6a20e9012b7fd4
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-15 20:33:00 -0300

    Update exercicio7.c
    Exercício calculo de area do retangulo

--- exercicios/exercicio7.c
+++ exercicios/exercicio7.c
@@ -1,1 +1,16 @@
 //Calcule a área de um retângulo. Area = comprimento * altura
+#include <stdio.h>
+
+int main(){
+    int altura, comprimento, area = 0;
+
+    printf("Para saber a area do retangulo, informe os valores solicitado:  \n");
+    printf("Altura: ");
+    scanf("%d", &altura);
+    printf("comprimento: ");
+    scanf("%d", &comprimento);
+    area = comprimento * altura;
+    printf("A area do círculo é %d cm", area);
+
+    return 0;
+}
